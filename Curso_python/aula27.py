14dbb5606a05cd7a07f1ae11133755ef47238894
lukask028 <54475600+lukask028@users.noreply.github.com>
2022-12-16 10:52:04 -0300

    Add files via upload

---
+++ Curso_python/aula27.py
@@ -0,0 +1,14 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321 de trás pra frente em posição 
+começo é o -9 == 0
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[::-1])
+#print(variavel[0:9:3])
+#percorre a variavel da posição 0 a 9 de 3 em 3
